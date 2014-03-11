ade446b82cbf4525c411efe2c4d28d1571cb572a
Ygor <ygor@Ygor-Inspiron-N4110.(none)>
2014-03-11 00:36:09 -0300

    linha pares e impares diferentes na tabela.

--- views/home.php
+++ views/home.php
@@ -10,32 +10,32 @@
 	$ptCtr = new PartidaController($db);
 	$localCtr = new LocalController($db);
 
-//	$partidas = $ptCtr->proximasPartidas("2013-03-10", 5);
-	$partidas = array();
+	$partidas = $ptCtr->proximasPartidas("2013-03-10", 5);
+	//$partidas = array();
 ?>
 
 <table class="table">
 
 	<tr>
 		<th>#</th>
-		<th>Partida:</th>
-		<th>Tipo:</th>
-		<th>Data:</th>
-		<th>Local:</th>
+		<th>Partida</th>
+		<th>Tipo</th>
+		<th>Data</th>
+		<th>Local</th>
 	</tr>
 
 	<?php
-		foreach ($partidas as $partida) {
+		foreach ($partidas as $key => $partida) {
 			$local = $localCtr->byId($partida->get("local_id"));
+			$color = ($key%2)?("#FF0000"):("#00FF00");
 	?>
-
-	<tr>
-		<td><?php echo $partida->get("id"); ?></td>
-		<td><?php echo $partida->get("nome"); ?></td>
-		<td><?php echo $partida->get("tipo"); ?></td>
-		<td><?php echo $partida->get("data"); ?></td>
-		<td><?php echo $local->get("nome"); ?></td>
-	</tr>
+			<tr bgcolor = <?php echo $color; ?> >
+				<td><?php echo $partida->get("id"); ?></td>
+				<td><?php echo $partida->get("nome"); ?></td>
+				<td><?php echo $partida->get("tipo"); ?></td>
+				<td><?php echo $partida->get("data"); ?></td>
+				<td><?php echo $local->get("nome"); ?></td>
+			</tr>
 
 	<?php
 		}
